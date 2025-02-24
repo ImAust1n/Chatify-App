9fc703a231121c6138068c1f650cca3ad26ee704
ImAust1n <austinmiracle007@gmail.com>
2025-02-24 14:48:11 +0530

    fix bug in message route

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,8 +1,8 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getUsersForSidebar, getMessages } from "../controllers/message.controller.js";
+import { getUsersForSidebar, getMessages, sendMessage } from "../controllers/message.controller.js";
 
-const router = experss.Router();
+const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
