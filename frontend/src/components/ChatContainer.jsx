9de5e545e98a8b02a83e489bf40dc258c7e6fdd7
ImAust1n <austinmiracle007@gmail.com>
2025-02-26 00:26:55 +0530

    Add sidebar component

---
+++ frontend/src/components/ChatContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChatContainer = () => {
+  return (
+    <div>
+      Chats
+    </div>
+  )
+}
+
+export default ChatContainer
---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,38 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import { axiosInstance } from "../lib/axios";
+
+export const useChatStore = create((set) => ({
+    messages: [],
+    users: [],
+    selectedUser: null,
+    isUserLoading: false,
+    isMessageLoading: false,
+
+    getUsers: async () => {
+        set({ isUserLoading: true });
+        try {
+            const res = await axiosInstance.get("/messages/users");
+            set({ users: res.data });
+        } catch (error) {
+            toast.error(error.reponse.data.message);
+        } finally {
+            set({ isUserLoading: false });
+        }
+    },
+
+    getMessages: async (userId) => {
+        set({ isMessageLoading: true });
+        try {
+            const res = await axiosInstance.get(`/messages/${userId}`);
+            set({ messages: res.data });
+        } catch (error) {
+            toast.error(error.reponse.data.message);
+        } finally {
+            set({ isMessageLoading: false });
+        }
+    },
+
+    // todo: Optimize this one 
+    setSelectedUser: (selectedUser) => set({ selectedUser }),
+}));
